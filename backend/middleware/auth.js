0ae3eb8f165e0f0f35c5e03b213198c06c92be39
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-31 17:09:06 +0000

    feat: added auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "There is no token, authorization denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.userId;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token it´s not valid" });
+  }
+};

668d635c8a647b7b1e9f62025424c34cbcb39a65
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-31 16:35:43 +0000

    build: setup mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
+//Get environments from .env file
+if (process.env.NODE_ENV !== "production") dotenv.config();
 
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(cors());

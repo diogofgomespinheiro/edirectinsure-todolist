fb39d078325b20948971872583cc8385db34e9f0
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-31 17:15:19 +0000

    fix: fixed auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
-
-  if (!token) {
-    return res
-      .status(401)
-      .json({ msg: "There is no token, authorization denied" });
-  }
-
   try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ msg: "There is no token, authorization denied" });
+    }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.userId = decoded.userId;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 // @route   GET users/me
 // @desc    Get user data
 // @access  Private
-router.get("/me", (req, res) => {
+router.get("/me", auth, (req, res) => {
   res.send("User Route Working");
 });
 
